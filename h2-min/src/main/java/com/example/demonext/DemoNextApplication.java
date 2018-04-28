14721cfbf7adb305075261f43d24db0877ae6989
bosik <bosikgn@gmail.com>
2018-04-29 02:38:59 +0300

    Minor code cleanup

--- h2-min/src/main/java/com/example/demonext/DemoNextApplication.java
+++ h2-min/src/main/java/com/example/demonext/DemoNextApplication.java
@@ -1,6 +1,5 @@
 package com.example.demonext;
 
-import com.example.demonext.model.Room;
 import com.example.demonext.services.RoomService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
